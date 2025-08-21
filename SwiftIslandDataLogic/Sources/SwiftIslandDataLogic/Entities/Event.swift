2150467c1f12b28ade1f67990710d3953ebf75bc
Niels van Hoorn <nvh@nvh.io>
2025-08-21 23:00:19 +0200

    Improved date range handling

--- SwiftIslandDataLogic/Sources/SwiftIslandDataLogic/Entities/Event.swift
+++ SwiftIslandDataLogic/Sources/SwiftIslandDataLogic/Entities/Event.swift
@@ -27,8 +27,8 @@ public struct Event {
     internal init(dbEvent: DBEvent, activity: Activity) {
         self.id = dbEvent.id
         self.startDate = dbEvent.startDate.start
-        self.endDate = dbEvent.startDate.start.addingTimeInterval(activity.duration)
-        self.duration = activity.duration
+        self.endDate = dbEvent.startDate.end ?? dbEvent.startDate.start.addingTimeInterval(activity.duration)
+        self.duration = self.endDate.timeIntervalSince(self.startDate)
         self.activity = activity
     }
 }
